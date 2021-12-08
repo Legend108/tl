c934e58834c40f91890054647013e149b2ecfb15
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-12-08 18:46:10 +0530

    Create navbar.php

---
+++ partials/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">CodeLead</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a href="/mydashboard/welcome.php" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="/mydashboard/login.php" class="nav-link">Login</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a href="/mydashboard/signup.php" class="nav-link">Sign Up</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a href="/mydashboard/logout.php" class="nav-link">Logout</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
