736edb83035d75ff19cdde984e648bd882bffa58
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-12-09 21:55:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
 
              $_SESSION['logged'] = true;
              $_SESSION['username'] = $username;
-            //  header('location: welcome.php');
+             header('location: welcome.php');
          }
       }
 ?>
@@ -73,7 +73,7 @@
     <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div> -->
-  <button type="submit" class="btn btn-outline-success mt-5">Sign Up</button>
+  <button type="submit" class="btn btn-outline-success mt-5">Log in</button>
 </form>
   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
